cd518b0f8679350cbe340246aebb7651ad6ba7b2
Sethu Prathapan <sethuprathapanmuthirayil@gmail.com>
2026-07-16 12:14:48 +0530

    working

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ from fastapi import FastAPI
 
 from app.api.v1.users import router as users_router
 
-# from app.api.v1.auth import router as auth_router
+from app.api.v1.auth import router as auth_router
+
 # from app.api.v1.tasks import router as tasks_router
 
 app = FastAPI(title="Synapze --The Task Manager", version="1.0.0")
 
 # app.include_router(chat_router, prefix="/api/v1", tags=["Chat"])
 app.include_router(users_router, prefix="/api/v1", tags=["Users"])
-# app.include_router(auth_router, prefix="/api/v1", tags=["Authentication"])
+app.include_router(auth_router, prefix="/api/v1", tags=["Authentication"])
 # app.include_router(tasks_router, prefix="/api/v1", tags=["Tasks"])
